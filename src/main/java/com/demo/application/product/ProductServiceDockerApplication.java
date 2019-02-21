f4af4c4acc28b76e4e1cb7115afa67bd4a0143b5
Suvendu Mandal <m_andal2002@yahoo.co.in>
2019-02-21 16:40:12 +0000

    "First Commit"

---
+++ src/main/java/com/demo/application/product/ProductServiceDockerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.application.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceDockerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo/application/product/exception/ProductNotfoundException.java
@@ -0,0 +1,5 @@
+package com.demo.application.product.exception;
+
+public class ProductNotfoundException extends RuntimeException {
+   private static final long serialVersionUID = 1L;
+}
